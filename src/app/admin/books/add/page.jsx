89f05bbd9e414e4c969534afa121a823ff9ccde8
deepakchandra4 <deepakchandra4551@gmail.com>
2025-06-17 13:22:10 +0530

    admin - folder structure added

---
+++ src/app/admin/books/add/page.jsx
@@ -0,0 +1,10 @@
+'use client'
+import React from 'react'
+
+export default function AddBook(){
+    return(
+        <>
+        <h1>Add Book</h1>
+        </>
+    )
+}
---
+++ src/app/admin/books/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BooksAdmin = () => {
+  return (
+    <div>BooksAdmin</div>
+  )
+}
+
+export default BooksAdmin
